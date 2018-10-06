80d87351033de34c5290b1460858730e8f78be36
Jeff <horusofoz@gmail.com>
2018-10-06 09:33:52 +0930

    * Uploading game code

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text storyText;
+    [SerializeField] Text actionText;
+    [SerializeField] TextMeshProUGUI titleText;
+    [SerializeField] Image image;
+    [SerializeField] State startingState;
+    State[] nextStates;
+
+    State state;
+
+    // Use this for initialization
+    void Start ()
+    {
+        state = startingState;
+        PresentState();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+
+        if(state != startingState)
+        {
+            titleText.gameObject.SetActive(false);
+        }
+        else
+        {
+            titleText.gameObject.SetActive(true);
+        }
+
+        var nextStates = state.GetNextStates();
+
+        if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            if (nextStates[0] != null)
+            {
+                state = nextStates[0];
+            }
+            else
+            {
+                Debug.Log("No first action to perform.");
+            }
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            if(nextStates[1] != null)
+            {
+                state = nextStates[1];
+            }
+            else
+            {
+                Debug.Log("No second action to perform.");
+            }
+        }
+
+        PresentState();
+    }
+
+    private void PresentState()
+    {
+        storyText.text = state.GetStateStory();
+        actionText.text = state.GetStateActions();
+        image.sprite = state.GetStateSprite();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="State")]
+public class State : ScriptableObject  {
+
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [TextArea(2, 4)] [SerializeField] string actionText;
+    [SerializeField] Sprite image;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public string GetStateActions()
+    {
+        return actionText;
+    }
+
+    public Sprite GetStateSprite()
+    {
+        return image;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
